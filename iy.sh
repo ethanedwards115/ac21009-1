c9e3f98f0b6163690973f18799a79fd406c35a7c
ethanedwards115 <ethanedwards115@github.com>
2020-11-03 14:33:15 +0000

    completed "add()" function

--- iy.sh
+++ iy.sh
@@ -10,11 +10,11 @@ initiy()
     if ! test -e ".iy.log"
     then
         touch ".iy.log"
-        echo "Repository created: $(date)" > .iy.log
+        echo "Repository created: $(date)." | tee -a .iy.log
 
     else
 
-        echo "Repository \"$1\" already exists"
+        echo "iy: repository \"$1\" already exists."
 
     fi
 }
@@ -29,15 +29,76 @@ mkrepo()
         mkdir $1
     fi
 
-    cd $1 > /dev/null
+    cd $1 > /dev/null 
     initiy $1
-    cd .. >/dev/null
+    cd .. > /dev/null
 
     return 0
 }
 
 ##
-# 
+# add a file to the repo
+##
+add()
+{
+
+    h="Usage: iy add [-c] <filename> <repository>"
+
+    while getopts ":hc:" opt; do
+        case ${opt} in
+
+            "c" )
+                if ! test -e $OPTARG
+                then
+                    touch $OPTARG
+                    mv "$OPTARG" "$3/$OPTARG"
+                    return 0
+
+                else
+                    echo "iy: file \"$OPTARG\" already exists."
+
+                fi
+                ;;
+            
+            "h" )
+                echo $h
+                return 0
+                ;;
+
+            \? )
+                echo "iy: option \"$OPTARG\" not recognised."
+                ./iy-help.sh
+
+                return 0
+                ;;
+
+            ":" )
+                echo "iy: -$OPTARG requires an argument.\n" 1>&2
+                echo "\n$h"
+                return 0
+                ;;
+
+        esac
+    done
+
+    if [ $# -lt 2 ]
+    then
+
+        echo $h
+
+    fi
+
+    if [ $# -eq 2 ]
+    then
+        mv $1 $2/$1
+
+    fi
+
+    return 0
+}
+
+##
+# checkout a file from the repo
 ##
 
 
