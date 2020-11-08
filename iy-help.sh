a899ab7d312f67ee2b2e55109a0926e036a451a5
Mara <mmsggatllo@dundee.ac.uk>
2020-11-08 16:33:23 +0000

    created help message

--- iy-help.sh
+++ iy-help.sh
@@ -1,1 +1,10 @@
-#!/bin/sh
+#!/usr/bin/bash
+
+echo "Usage:"
+   echo "   bash iy.sh <command> [options] [args] - standard format for commands."
+   echo "   mkrepo <rep_name> - create a new repository."
+   echo "   add [-c] <file> <repository> - add a new file to the repository. -c creates the file if it doesn\`t already exist."
+   echo "   checkout <file> <repository> [location] - checkout a file from the repository (location - directory where the checked-out file should go)."
+   echo "   showrepo <repository> - display the contents of the repository."
+   echo "   help - show this help screen."
+   echo "   backup <repository> - manually backup a repository. This should be done automatically."
